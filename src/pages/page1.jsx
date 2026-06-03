9bc03d66f1f90269b16d88a5bc5106bcdc16e244
PritiHM <pritimali2806@gmail.com>
2026-06-03 17:33:54 +0530

    Initial commit

--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -4,8 +4,8 @@ import ProposalContent from "../components/ProposalContent";
 
 const Page1 = ({ data, quoteNo }) => {
   return (
-    <div className="w-full max-w-[210mm] min-h-[297mm] mx-auto bg-white px-4 md:px-8 py-4 md:py-6 shadow-lg overflow-x-auto">
-      <Header />
+<div className="w-[210mm] h-[297mm] mx-auto bg-white px-8 py-6 overflow-hidden">
+        <Header />
 
       <CustomerInfo
         data={data}
--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -2,7 +2,7 @@ import TechnicalDetails from "../components/TechnicalDetails";
 
 const Page2 = () => {
   return (
-<div className="w-full max-w-[210mm] min-h-[297mm] mx-auto bg-white px-4 md:px-8 py-4 md:py-6 shadow-lg overflow-x-auto">      <TechnicalDetails />
+<div className="w-[210mm] h-[297mm] mx-auto bg-white px-8 py-6 overflow-hidden">        <TechnicalDetails />
     </div>
   );
 };
