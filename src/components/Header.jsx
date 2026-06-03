4ef4e72ba991e074d36e89dbe398f6d78ab28943
PritiHM <pritimali2806@gmail.com>
2026-06-03 16:32:12 +0530

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,29 +7,29 @@ const Header = () => {
     <div className="pb-4 w-full max-w-full overflow-hidden">
 
       {/* Logos Row */}
-      <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-between sm:items-center sm:gap-0 w-full mb-6">
+      <div className="flex flex-row justify-between items-center w-full mb-6">
 
-        <div className="w-full sm:w-1/3 flex justify-center sm:justify-start">
+        <div className="w-1/3 flex justify-start">
           <img
             src={tmtl}
             alt="TMTL"
-            className="h-12 sm:h-16 object-contain"
+            className="h-10 sm:h-16 object-contain"
           />
         </div>
 
-        <div className="w-full sm:w-1/3 flex justify-center">
+        <div className="w-1/3 flex justify-center">
           <img
             src={nexus}
             alt="Nexus"
-            className="h-16 sm:h-20 object-contain"
+            className="h-12 sm:h-20 object-contain"
           />
         </div>
 
-        <div className="w-full sm:w-1/3 flex justify-center sm:justify-end">
+        <div className="w-1/3 flex justify-end">
           <img
             src={perfect}
             alt="Perfect"
-            className="h-12 sm:h-16 object-contain"
+            className="h-10 sm:h-16 object-contain"
           />
         </div>
 
@@ -44,11 +44,17 @@ const Header = () => {
       </p>
 
       <div className="text-center text-xs mt-1 px-4 text-gray-900 flex flex-wrap justify-center gap-x-2 gap-y-0.5">
-        <span className="inline-block">Sales: +91 8668250720 / 8983328537</span>
+        <span className="inline-block">
+          Sales: +91 8668250720 / 8983328537
+        </span>
         <span className="hidden sm:inline">|</span>
-        <span className="inline-block">Email: sales@nexusurjasolutions.com</span>
+        <span className="inline-block">
+          Email: sales@nexusurjasolutions.com
+        </span>
         <span className="hidden sm:inline">|</span>
-        <span className="inline-block">GST: 27EPUPM4088H1Z2</span>
+        <span className="inline-block">
+          GST: 27EPUPM4088H1Z2
+        </span>
       </div>
 
       <hr className="mt-3 border-gray-400" />
