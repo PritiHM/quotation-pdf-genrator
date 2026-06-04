37b48dc407cab815e628fb8d12ecfcc7b638f767
PritiHM <pritimali2806@gmail.com>
2026-06-04 14:54:34 +0530

    Initial commit

--- src/pages/page4.jsx
+++ src/pages/page4.jsx
@@ -3,14 +3,12 @@ import TermsConditions from "../components/TermsConditions";
 
 const Page4 = ({ data }) => {
   return (
-    <div className="page-container w-full max-w-[210mm] min-h-[297mm] mx-auto bg-white px-8 py-6">
-
+    <div className="page-container w-full max-w-[210mm] min-h-[297mm] mx-auto bg-white px-8 py-4">
       <CommercialTable data={data} />
 
-      <div className="mt-8">
+      <div className="mt-3">
         <TermsConditions />
       </div>
-
     </div>
   );
 };
